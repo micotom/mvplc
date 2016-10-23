4ca7f71d2f4841b4da21e981f8e4a5f0dd6ec5de
micotom <funglejunk.development@googlemail.com>
2016-10-23 20:51:36 +0200

    move presenter impl to inner class in test class

--- mvplc/src/test/java/com/bragi/mvplc/components/MvplcComponentTest.java
+++ mvplc/src/test/java/com/bragi/mvplc/components/MvplcComponentTest.java
@@ -2,6 +2,7 @@ package com.bragi.mvplc.components;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -94,6 +95,19 @@ public class MvplcComponentTest {
         @Override
         public void onFragmentDestroyed() {
             status = FragmentLifecycleEvent.DESTROYED;
+        }
+    }
+
+    private static class MvplcPresenterImpl implements MvplcPresenter {
+        FragmentLifecycleEvent status;
+        public MvplcPresenterImpl(){};
+        @Override
+        public void start(@NonNull MvplcFragment fragment) {
+            status = FragmentLifecycleEvent.STARTED;
+        }
+        @Override
+        public void stop() {
+            status = FragmentLifecycleEvent.STOPPED;
         }
     }
 
